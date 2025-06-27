0a34a9f13bb4ff7bc38163def503bb6f775484da
devathrv <shivanshshukla908@gmail.com>
2025-06-27 19:33:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const hamburger = document.getElementById('hamburger');
+const overlayMenu = document.getElementById('overlayMenu');
 
+hamburger.addEventListener('click', () => {
+  if (overlayMenu.style.left === '0%') {
+    overlayMenu.style.left = '-100%';
+    hamburger.innerHTML = '&#9776;';
+  } else {
+    overlayMenu.style.left = '0%';
+    hamburger.innerHTML = '&times;';
+  }
+});
+  
